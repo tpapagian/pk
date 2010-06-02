5b60d44dec94ed866b81b6081c8c3c098b6bab5b
alex <alex@tom.csail.mit.edu>
2010-06-02 17:51:00 -0400

    add more events

--- arch/x86/include/asm/syscount.h
+++ arch/x86/include/asm/syscount.h
@@ -1,6 +1,11 @@
-extern void syscount_add(unsigned long call, struct timespec start, struct timespec stop);
+void syscount_add(unsigned long call, struct timespec start, struct timespec stop);
 extern void syscount_end_task(struct task_struct *tsk);
 
+#define SYSCOUNT_MNT_PUT1      500
+#define SYSCOUNT_MNT_PUT2      501
+#define SYSCOUNT_FPUT          502
+#define SYSCOUNT_IXGBE_RX      503
+#define SYSCOUNT_IXGBE_TX      504
 #define SYSCOUNT_DUP_MMAP_ANON 505
 #define SYSCOUNT_DUP_MMAP_MPOL 506
 #define SYSCOUNT_DUP_MMAP_STAR 507
