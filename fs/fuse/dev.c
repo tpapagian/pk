b1b993c881f9d1d5e445fd1b5da7749068f150e0
Silas Boyd-Wickizer <sbw@kid.csail.mit.edu>
2010-08-10 02:02:10 -0400

    page: fix compile error with fuse and hugetlb

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -643,15 +643,14 @@ static int fuse_check_page(struct page *page)
 	if (page_mapcount(page) ||
 	    page->mapping != NULL ||
 	    page_count(page) != 1 ||
-	    (page->flags & PAGE_FLAGS_CHECK_AT_PREP &
-	     ~(1 << PG_locked |
-	       1 << PG_referenced |
+	    (page->flags_ & PAGE_FLAGS_CHECK_AT_PREP &
+	     ~(1 << PG_referenced |
 	       1 << PG_uptodate |
 	       1 << PG_lru |
 	       1 << PG_active |
 	       1 << PG_reclaim))) {
 		printk(KERN_WARNING "fuse: trying to steal weird page\n");
-		printk(KERN_WARNING "  page=%p index=%li flags=%08lx, count=%i, mapcount=%i, mapping=%p\n", page, page->index, page->flags, page_count(page), page_mapcount(page), page->mapping);
+		printk(KERN_WARNING "  page=%p index=%li flags=%08lx, count=%i, mapcount=%i, mapping=%p\n", page, page->index, page->flags_, page_count(page), page_mapcount(page), page->mapping);
 		return 1;
 	}
 	return 0;
