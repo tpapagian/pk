2ce80c38447a86597c6ac1df53a7d73518935a42
Silas Boyd-Wickizer <sbw@wether.csail.mit.edu>
2010-12-27 23:36:20 -0800

    [branch: mtrace]
    Add CONFIG_MTRACE, but not fully implemented yet.

--- include/linux/mtrace.h
+++ include/linux/mtrace.h
@@ -1,6 +1,8 @@
 #ifndef _LINUX_MTRACE_H_
 #define _LINUX_MTRACE_H_
 
+#ifdef CONFIG_MTRACE
+
 extern void mtrace_init(void);
 extern void mtrace_init_task(struct task_struct *t);
 extern void mtrace_exit_task(struct task_struct *t);
@@ -12,5 +14,7 @@ struct mtrace_call {
 	u64 tag;
 	u64 pc;
 };
+
+#endif /* CONFIG_MTRACE */
 
 #endif
